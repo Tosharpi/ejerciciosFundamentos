c4ef5882f9bca682a949b6841c71978d47c76361
Tosharpi <git config --global user.email exmple@gmail.com>
2024-03-01 14:47:45 -0500

    area del circulo y pitagoras

---
+++ pitagoras.py
@@ -0,0 +1,9 @@
+
+#pitagoritas
+
+cateto_opuesto = float(input('ingrese el cateto opuesto: '))
+cateto_adyacente = float(input('ingrese el cateto adyacente: '))
+
+hipotenusa = (cateto_adyacente + cateto_opuesto**2)**(1/2)
+
+print("la hipotenusa es: ",hipotenusa)
