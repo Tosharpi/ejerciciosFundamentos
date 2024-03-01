aa0b2c948a9ed48b2513fab156c73a9ad1a481a1
Tosharpi <git config --global user.email exmple@gmail.com>
2024-03-01 13:23:06 -0500

    promedio

---
+++ promedio.py
@@ -0,0 +1,7 @@
+#promedio de 4 notas que ingrese el usuario
+
+nota1 = int(input('ingrese la nota #1 ')); nota2 = int(input('ingrese la nota #2 ')); nota3 = int(input('ingrese la nota #3 ')); nota4 = int(input('ingrese la nota # 4: '))
+sumaNotas = nota1 + nota2 + nota3 + nota4
+promedio = sumaNotas/4
+
+print('su pomedio es: ', promedio)
